d25c27b48fe4ebf3e45ec2a14c256d02536e8640
sahadat20 <shahadathossainroni941@gmail.com>
2024-08-07 09:13:52 +0600

    Contract 1st stage"

---
+++ contracts/BankAccount copy.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <=0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+contract BankAccount {
+    event Deposit(
+        address indexed user,
+        uint indexed accountId,
+        uint value,
+        uint timestamp
+    );
+    event WithdrawRequested(
+        address indexed user,
+        uint indexed accountId,
+        uint indexed withdrawId,
+        uint amount,
+        uint timestamp
+    );
+    // for successfully withdraw
+    event Withdraw(uint indexed withdrawId, uint timestamp);
+    event AccountCreated(address[] owners, uint indexed id, uint timestamp);
+
+    struct WithdrawRequest {
+        address user;
+        uint amount;
+        uint approvals;
+        mapping(address => bool) ownersApproved;
+        bool approved;
+    }
+
+    struct Account {
+        address[] owners;
+        uint balance;
+        mapping(uint => WithdrawRequest) withdrawRequests;
+    }
+
+    mapping(uint => Account) accounts;
+    mapping(address => uint[]) userAccounts;
+    uint nextAccountId;
+    uint nextWithdrawId;
+
+    modifier accountOwner(uint accountId) {
+        bool isOwner;
+        for (uint idx; idx < accounts[accountId].owners.length; idx++) {
+            if (accounts[accountId].owners[idx] == msg.sender) {
+                isOwner = true;
+                break;
+            }
+        }
+        require(isOwner, "you are not owner of this account");
+        _;
+    }
+
+    function deposit(uint accountId) external payable accountOwner(accountId) {
+        accounts[accountId].balance += msg.value;
+    }
+
+    function createAccount(address[] calldata otherOwners) external {}
+
+    function requestWithdrawl(uint accountId, uint amount) external {}
+
+    function approveWithdrawl(uint accountId, uint withdrawId) external {}
+
+    // to withdraw amount into the requister account
+    function withdraw(uint accountId, uint withdrawId) external {}
+
+    function getBalance(uint accountId) public view returns (uint) {}
+
+    function getOwners(uint accountId) public view returns (address[] memory) {}
+
+    // return number of approvar
+    function getApprovals(
+        uint accountId,
+        uint withdrawId
+    ) public view returns (uint) {}
+
+    function getAccounts() public view returns (uint[] memory) {}
+}
